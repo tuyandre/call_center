c3552e31ccd4b192795cc1f163a22bd65b42abee
Igor J.L. Ndiramiye <nigorjeanluc@gmail.com>
2022-08-08 17:35:28 +0200

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\CallLogs;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +16,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        CallLogs::factory(100)->create();
     }
 }
